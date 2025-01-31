ec82fffc862b0cbbfdd92f4fb7f4897180d57512
Ahmed-Basha1 <ahmedybasha1@outlook.com>
2025-01-31 15:20:38 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'YouSSSSSSSSSSSSSSSSSSSSs have pushed the button this SSSSSSSSSSSSSSSSSSSSSSSSSSSSSS times:',
+              'Final       ::  hhhh YouSSSSSSSSSSSSSSSSSSSSs have pushed the button this SSSSSSSSSSSSSSSSSSSSSSSSSSSSSS times:',
             ),
             Text(
               '$_counter',
